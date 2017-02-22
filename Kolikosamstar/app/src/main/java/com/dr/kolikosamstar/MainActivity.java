f777a3490b62ef66231cab2d2b9603eaf406a739
noNameDev4 <notValid@email.hr>
2017-02-22 18:57:28 +0100

    When you code 15 minutes a day
    Start of coding

--- Kolikosamstar/app/src/main/java/com/dr/kolikosamstar/MainActivity.java
+++ Kolikosamstar/app/src/main/java/com/dr/kolikosamstar/MainActivity.java
@@ -2,6 +2,8 @@ package com.dr.kolikosamstar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Izracunaj button */
+    public void calculateAge(View view) {
+        EditText editText = (EditText) findViewById(R.id.edittext_year);
+        String year = editText.getText().toString();
     }
 }
