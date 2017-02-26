2cb9606f9e4c8a22b85a81123fb4a516619e3552
noNameDev4 <notValid@email.hr>
2017-02-26 21:01:03 +0100

    5 AM coding with limted internet access
    Input readers

--- Kolikosamstar/app/src/main/java/com/dr/kolikosamstar/MainActivity.java
+++ Kolikosamstar/app/src/main/java/com/dr/kolikosamstar/MainActivity.java
@@ -1,9 +1,14 @@
 package com.dr.kolikosamstar;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +20,27 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user clicks the Izracunaj button */
     public void calculateAge(View view) {
+        //Hide keyboard
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
         EditText editText = (EditText) findViewById(R.id.edittext_year);
         String year = editText.getText().toString();
+        TextView answer= (TextView) findViewById(R.id.textViewResult);
+
+        Spinner spinnerDay = (Spinner) findViewById(R.id.spinner_days);
+       // String day = spinner.getSelectedItem().(); // Small, Medium, Large
+
+
+        int spinner_pos = spinnerDay.getSelectedItemPosition();
+       // String[] size_values = getResources().getStringArray(R.array.size_values);
+       // int size = Integer.valueOf(size_values[spinner_pos]);
+        Spinner spinnerMonths = (Spinner) findViewById(R.id.spinner_months);
+        int spinner_pos = spinnerDay.getSelectedItemPosition();
+
+        answer.setText(year + spinner_pos + );
+
     }
 }
